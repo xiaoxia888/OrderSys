0a372e3b6ff872f03fdca932cdaa00944dd7465d
xiaoxia888 <xgx20001222@163.com>
2022-08-27 09:57:50 +0800

    小更

--- src/main/java/com/chinasofti/ordersys/service/DomainProtectedService.java
+++ src/main/java/com/chinasofti/ordersys/service/DomainProtectedService.java
@@ -1,54 +1,24 @@
 /*    */ package com.chinasofti.ordersys.service;
-/*    */ 
-/*    */ import com.chinasofti.web.common.httpequest.HttpRequestContext;
-/*    */ import javax.servlet.http.HttpServletRequest;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class DomainProtectedService
-/*    */ {
-/*    */   public boolean isFromSameDomain() {
-/* 36 */     HttpServletRequest request = HttpRequestContext.getRequest();
-/*    */     
-/* 38 */     String path = request.getContextPath();
-    System.out.println(path);
-/*    */     
-/* 40 */     String basePath = String.valueOf(request.getScheme()) + "://" + request.getServerName() + 
-/* 41 */       ":" + request.getServerPort() + path + "/";
-/*    */     
-/* 43 */     String fromUrl = request.getHeader("referer");
-/*    */     
-/* 45 */     return (fromUrl != null && fromUrl.startsWith(basePath));
-/*    */   }
-/*    */ }
+
+import com.chinasofti.web.common.httpequest.HttpRequestContext;
+import javax.servlet.http.HttpServletRequest;
 
 
-/* Location:              C:\Users\xgx\Desktop\点餐系统\apache-tomcat-9.0.64\webapps\OrderSys\WEB-INF\classes\!\com\chinasofti\ordersys\service\DomainProtectedService.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+
+
+public class DomainProtectedService {
+  public boolean isFromSameDomain() {
+    HttpServletRequest request = HttpRequestContext.getRequest();
+
+    String path = request.getContextPath();
+    //System.out.println(path);
+
+    String basePath = String.valueOf(request.getScheme()) + "://" + request.getServerName() +
+      ":" + request.getServerPort() + path + "/";
+
+    String fromUrl = request.getHeader("referer");
+
+    return (fromUrl != null && fromUrl.startsWith(basePath));
+  }
+}
+
